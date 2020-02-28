fd3292561edf2e19ceabeead2fed76835c9b1267
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-28 18:21:11 +0530

    added seed data for currencies

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_115612) do
   create_table "currencies", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.bigint "maxsupply"
+    t.bigint "max_supply"
     t.string "currency_symbol"
     t.string "slug"
     t.datetime "created_at", precision: 6, null: false
