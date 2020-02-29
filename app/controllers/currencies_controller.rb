ea790697670459235e64ab6d55914ff3e65bb1b6
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-29 13:59:18 +0530

    add currency search method

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,6 +3,8 @@ class CurrenciesController < ApplicationController
 	end
 
 	def search
+		@currencies = Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
+		render json: { currencies: @currencies }
 	end
 
 	def calculate
