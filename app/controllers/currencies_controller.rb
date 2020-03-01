762e497f29d8307632bfc3f21c1e0d5a55b3a6e9
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-01 12:56:42 +0530

    add currency calculate method

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -6,7 +6,21 @@ class CurrenciesController < ApplicationController
 		@currencies = Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
 		render json: { currencies: @currencies }
 	end
-
+	
+	# Takes in the currency id and the amount owned
+  # Returns final calculations
 	def calculate
+		amount = params[:amount]
+    render json: {
+      currency: currency,
+      current_price: currency.current_price,
+      amount: amount,
+      value: currency.calculate_value(amount)
+    }
 	end 
+
+	private 
+	def currency 
+		@currency = Currency.find(params[:id])
+	end
 end
