5f387298662034aebde4d5cd230f72aaccaf7a13
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-01 14:51:08 +0530

    set up React

---
+++ app/javascript/components/PortfolioContainer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PortfolioContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      portolio: [],
+      searchResults: [],
+      activeCurrency: null,
+      amount: null
+    };
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default PortfolioContainer;
